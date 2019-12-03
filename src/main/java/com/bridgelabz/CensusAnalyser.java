92a2cd7d21aa8efbc4ee002d5e0bf01e3ee1d5da
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-03 19:40:56 +0530

    Add:Test Code for Sort the CSV by StateName and write into json

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,56 +1,65 @@
 package com.bridgelabz;
+import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 public class CensusAnalyser {
 
-    public int OpenCSVBuilder(Reader reader, Object POJO)  {
-        int noOfRecord = 0;
-        CsvToBean csvToBean = new CsvToBeanBuilder(reader)
-                .withType(POJO.getClass())
-                .withIgnoreLeadingWhiteSpace(true)
-                .build();
-        Iterator<Object> csvUserIterator = csvToBean.iterator();
-        while (csvUserIterator.hasNext()) {
-            csvUserIterator.next();
-            noOfRecord++;
+    private static String SAMPLE_CSV_FILE_PATH ="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCode.JSON";
+
+    public int checkRecordStateCensusCSV(String SAMPLE_CSV_FILE_PATH) throws CSVFileException, IOException {
+        int count = 0;
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+        ) {
+            CsvToBean<StateCensusCSV> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(StateCensusCSV.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            List<StateCensusCSV> StateData=new ArrayList<>();
+
+            Iterator<StateCensusCSV> statesIterator = csvToBean.iterator();
+            while (statesIterator.hasNext()) {
+                count++;
+                StateCensusCSV csvStates = statesIterator.next();
+                StateData.add(csvStates);
+            }
+            sortingStateByName(StateData);
+            writeIntoJSON(StateData);
+
+        } catch (NoSuchFileException e) {
+            throw new CSVFileException("NO_SUCH_FILE", CSVFileException.ExceptionType.NO_SUCH_FILE);
+        } catch (RuntimeException e) {
+            throw new CSVFileException("Runtime Error", CSVFileException.ExceptionType.BINDING_ERROR);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return  noOfRecord;
-
+        return count;
     }
 
-    public int checkNumberOfRecordsStateCSV(String SAMPLE_CSV_FILE_PATH,Object POJO) throws CSVFileException, IOException {
-        int record=0;
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-            record = OpenCSVBuilder(reader, POJO);
-        } catch (NoSuchFileException e) {
-            throw new CSVFileException("No such file or InCorrect file type", CSVFileException.ExceptionType.NO_SUCH_FILE);
-        } catch (RuntimeException e) {
-            throw new CSVFileException("runtime error", CSVFileException.ExceptionType.BINDING_ERROR);
-        }
-
-        return record;
+    private static void sortingStateByName(List<StateCensusCSV> list) {
+        Comparator<StateCensusCSV> data = Comparator.comparing(StateCensusCSV::getState);
+        list.sort(data);
+        System.out.println(list.toString());
     }
 
-    public int checkRecordCensusCSV(String SAMPLE_CSV_FILE_PATH, Object POJO) throws IOException, CSVFileException {
-        int recordCount = 0;
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-            recordCount = OpenCSVBuilder(reader, POJO);
-        } catch (NoSuchFileException e) {
-            throw new CSVFileException("enter proper file name", CSVFileException.ExceptionType.NO_SUCH_FILE);
-        } catch (RuntimeException e) {
-            throw new CSVFileException("runtime error", CSVFileException.ExceptionType.BINDING_ERROR);
-        }
-        return recordCount;
+    private static void writeIntoJSON(List<StateCensusCSV> list) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        FileWriter writer = new FileWriter(SAMPLE_CSV_FILE_PATH);
+        writer.write(json);
+        writer.close();
     }
 }
-
--- src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
@@ -5,69 +5,20 @@ import org.junit.Test;
 import java.io.IOException;
 public class StateCensusAnalyserTest {
     CensusAnalyser statesCensusAnalyzer = new CensusAnalyser();
-    StateCSV stateCSV=new StateCSV();
-    StateCensusCSV stateCensusCSV=new StateCensusCSV();
-
-    private static String SAMPLE_CSV_FILE_PATH="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCode.csv";
-    private static String SAMPLE_CSV_FILE_DUMMY="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCode1.csv";
 
     private  static String STATE_CENSUS_DATA_FILE_PATH="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCensusData.csv";
     private static String STATE_CENSUS_DATA_FILE_PATHDUMMY="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCensusData.txt";
     private static String STATE_CENSUS_DATA_DUMMY="/home/admin1/Desktop/CensusAnalyserProblem/src/test/resources/StateCensusDataDummy.csv";
-    @Test
-    public void givenMethod_CheckNumberOfRecordesMatchesIfmatches_ShouldReturnTrue() throws IOException {
-        try {
-            int getRecords = statesCensusAnalyzer.checkNumberOfRecordsStateCSV(SAMPLE_CSV_FILE_PATH,stateCSV);
-            Assert.assertEquals(37, getRecords);
-        }catch (CSVFileException e) {
-            e.printStackTrace();
-        }
-    }
-    @Test
-    public void givenMethode_FoundIncorrectFileName_ThrowsException() throws IOException {
-        try {
-             statesCensusAnalyzer.checkNumberOfRecordsStateCSV(SAMPLE_CSV_FILE_DUMMY,stateCSV);
-        }catch (CSVFileException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CSVFileException.ExceptionType.NO_SUCH_FILE,e.type);
-        }
-    }
-    @Test
-    public void givenMethod_FoundInCorrectFileType_ThrowsException() throws IOException {
-        try{
-            statesCensusAnalyzer.checkNumberOfRecordsStateCSV(SAMPLE_CSV_FILE_DUMMY,stateCSV);
-        }catch(CSVFileException  e){
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CSVFileException.ExceptionType.NO_SUCH_FILE, e.type);
-        }
-    }
-    @Test
-    public void givenMethod_FoundInCorrectDelimeter_ThrowsException() throws IOException {
-        try{
-            statesCensusAnalyzer.checkNumberOfRecordsStateCSV(SAMPLE_CSV_FILE_PATH,stateCSV);
-        }catch (CSVFileException e){
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CSVFileException.ExceptionType.BINDING_ERROR,e.type);
-        }
-    }
-    @Test
-    public void givenMethod_FoundIncCorrectHeader_ThrowsException() throws IOException {
-        try {
-            statesCensusAnalyzer.checkNumberOfRecordsStateCSV(SAMPLE_CSV_FILE_PATH,stateCSV);
-        } catch (CSVFileException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CSVFileException.ExceptionType.BINDING_ERROR, e.type);
-        }
-    }
+
     @Test
     public void givenMethod_checkNumberOfRecordsMatchesOrNot() throws CSVFileException, IOException {
-            int getRecords = statesCensusAnalyzer.checkRecordCensusCSV(STATE_CENSUS_DATA_FILE_PATH,stateCensusCSV);
+            int getRecords = statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_FILE_PATH);
             Assert.assertEquals(29, getRecords);
         }
     @Test
     public void givenMethod_FoundInCorrectFileName_ThrowsException() throws IOException {
         try {
-            statesCensusAnalyzer.checkRecordCensusCSV(STATE_CENSUS_DATA_FILE_PATHDUMMY,stateCensusCSV);
+            statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_FILE_PATHDUMMY);
         }catch (CSVFileException e){
             System.out.println(e.getMessage());
             Assert.assertEquals(CSVFileException.ExceptionType.NO_SUCH_FILE,e.type);
@@ -76,7 +27,7 @@ public class StateCensusAnalyserTest {
     @Test
     public void givenMethod_IfFoundInCorrectFileType_ThrowsException() throws IOException {
         try{
-            statesCensusAnalyzer.checkRecordCensusCSV(STATE_CENSUS_DATA_FILE_PATHDUMMY,stateCensusCSV);
+            statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_FILE_PATHDUMMY);
         }catch(CSVFileException  e){
             System.out.println(e.getMessage());
             Assert.assertEquals(CSVFileException.ExceptionType.NO_SUCH_FILE, e.type);
@@ -85,7 +36,7 @@ public class StateCensusAnalyserTest {
     @Test
     public void givenMethod_FoundInCorrectDelimeterInFile_ThrowsException() throws IOException {
         try{
-            statesCensusAnalyzer.checkRecordCensusCSV(STATE_CENSUS_DATA_DUMMY,stateCensusCSV);
+            statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_DUMMY);
         }catch (CSVFileException e){
             System.out.println(e.getMessage());
             Assert.assertEquals(CSVFileException.ExceptionType.BINDING_ERROR,e.type);
@@ -94,10 +45,15 @@ public class StateCensusAnalyserTest {
     @Test
     public void givenMethod_FoundInCorrectHeaderInFile_ThrowsException() throws IOException {
         try{
-            statesCensusAnalyzer.checkRecordCensusCSV(STATE_CENSUS_DATA_DUMMY,stateCensusCSV);
+            statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_DUMMY);
         }catch (CSVFileException e){
             System.out.println(e.getMessage());
             Assert.assertEquals(CSVFileException.ExceptionType.BINDING_ERROR,e.type);
         }
+    }
+    @Test
+    public void givenMethod_ForSortTheCSVAndWriteIntoJson() throws CSVFileException, IOException {
+        int records=statesCensusAnalyzer.checkRecordStateCensusCSV(STATE_CENSUS_DATA_FILE_PATH);
+        Assert.assertEquals(29,records);
     }
  }
